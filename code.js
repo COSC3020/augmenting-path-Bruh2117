723fdc527112bcf1f0232a10f018e54396a48620
Aidan Giraldo <agirald1@uwyo.edu>
2023-12-11 19:03:48 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
-function augmentingPath(graph, start, end) {
+function augmentingPath(graph, start, end, path = []) 
+{
+    path.push(start);
+
+    if (start == end) 
+    {
+        return path;
+    }
+
+    for (var node in graph[start]) 
+    {
+        if (path.includes(node) == false) 
+        {
+            let augPath = augmentingPath(graph, node, end, path);
+        
+            if (augPath.length > 0) 
+            {
+                return augPath; 
+            }
+            else 
+            {
+                path = []; 
+                path.push(start);
+            }
+        }
+    }
+
     return [];
 }
